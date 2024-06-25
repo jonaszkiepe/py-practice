40a94dfb888b56eeae4cc5a19603edac3b5d4c4d
Jonasz Kiepe <loafofbread63@gmail.com>
2024-06-25 18:33:43 +0200

    optimized tic tac toe a bit

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,75 +1,46 @@
 import os
-import random
-
-
-def clear():
-    os.system("clear")
-
 
 positions = [["123", "456", "789"],
              ["147", "258", "369"],
              ["159", "357"]]
 
 
-def check_win():
+def check_win(player):
     for position in positions:
         for chars in position:
             if "xxx" in chars or "ooo" in chars:
+                print(f"player {player} won!")
                 return True
 
 
 def gen_board():
+    os.system("clear")
     board = ""
     for row in positions[0]:
         for char in row:
-            if char not in ["x", "o"]:
-                board += " " + "|"
-            else:
-                board += char + "|"
-        board = board[:-1]
-        board += "/-----/"
-    for row in board.split("/")[:-2]:
-        print(row)
+            if char not in "xo": board += " |"
+            else: board += char + "|"
+        board = board[:-1] + "/-----/"
+    for row in board.split("/")[:-2]: print(row)
 
 
 def replace_number(replace, player):
-    if player == 1:
-        symbol = "x"
-    if player == 2:
-        symbol = "o"
-    for position in positions:
-        for chars in position:
-            index1 = positions.index(position)
-            index2 = position.index(chars)
-            positions[index1][index2] = chars.replace(replace, symbol)
-
-
-def switch_player(player):
-    if player == 1:
-        return 2
-    if player == 2:
-        return 1
+    if player == 1: symbol = "x"
+    elif player == 2: symbol = "o"
+    for k in range(len(positions)):
+        for j in range(len(positions[k])):
+            positions[k][j] = positions[k][j].replace(replace, symbol)
 
 
 def main():
-    clear()
-    player = 1
-    turns = 0
-    while turns < 9:
-        turns += 1
-        print(f"player {player} turn")
+    switch, player = 1, 1
+    for i in range(0, 9):
         gen_board()
-        replace_number(input(), player)
-        if check_win():
-            break
-        player = switch_player(player)
-        clear()
-    clear()
+        if check_win(player): return
+        replace_number(input(f"Player {player} turn: "), player)
+        switch, player = switch * -1, player + switch
     gen_board()
-    if turns == 9:
-        print("\ndraw")
-    else:
-        print(f"\nplayer {player} won!")
+    print("Draw")
 
 
 if __name__ == "__main__":
